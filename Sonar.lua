ffe8eed23321460ca5e869dc04ac906bc6e0858c
Gabriel Arantes <gabriel_arantes18@hotmail.com>
2018-06-23 20:09:10 -0300

    Turning 3 directions

--- Sonar.lua
+++ Sonar.lua
@@ -10,11 +10,12 @@ if (sim_call_type==sim.syscb_init) then
     noDetectionDist=1.45
     estadoDeOperacao="Segue a parede"
     turnTime=0
-    turnDirection=1
+    turnDirection=0
     detect={0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}
     --braitenbergL={-0.2,-0.4,-0.6,-0.8,-1,-1.2,-1.4,-1.6, 0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0}
     --braitenbergR={-1.6,-1.4,-1.2,-1,-0.8,-0.6,-0.4,-0.2, 0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0}
     v0=2
+    minDist=0.5
 end 
 
 if (sim_call_type==sim.syscb_cleanup) then 
@@ -31,15 +32,17 @@ if (sim_call_type==sim.syscb_actuation) then
         end
     end
 
-    sim.addStatusbarMessage(estadoDeOperacao)
+    Msg=string.format("Dist8: %.4f ### Dist9: %.4f",detect[8],detect[9])
+    sim.addStatusbarMessage(Msg .. estadoDeOperacao)
+    
 
     if (estadoDeOperacao=="Segue a parede") then
     
         vLeft=v0
         vRight=v0
   
-        --Msg=string.format("Dist8: %.4f ### Dist9: %.4f",detect[8],detect[9])
-        --sim.addStatusbarMessage(Msg)
+        Msg=string.format("Dist8: %.4f ### Dist9: %.4f",detect[8],detect[9])
+        sim.addStatusbarMessage(Msg)
         
         if (detect[9]==0) or (detect[16]==0) then
             estadoDeOperacao="Faz a curva"
@@ -51,9 +54,8 @@ if (sim_call_type==sim.syscb_actuation) then
        
         if(detect[8]>detect[9])then
             vRight=vRight-0.2
-        else if (detect[8]<detect[9]) then
+        elseif (detect[8]<detect[9]) then
             vLeft=vLeft-0.2
-        end
         end
         
     end
@@ -67,7 +69,7 @@ if (sim_call_type==sim.syscb_actuation) then
             turnTime=turnTime+1
         else
             vRight=v0
-            if(detect[4]<0.5) or (detect[5]<0.5) or (detect[6]<0.5) or (detect[7]<0.5) then
+            if(detect[4]<minDist) or (detect[5]<minDist) or (detect[6]<minDist) or (detect[7]<minDist) then
                 vLeft=0
                 if(turnTime<110) then
                     turnTime=turnTime+1
@@ -84,17 +86,21 @@ if (sim_call_type==sim.syscb_actuation) then
         if (turnDirection==0) then
             vLeft=v0
             vright=v0
-        end
-        if (turnDirection==1) then
-            vLeft=3.4
-            vRight=1.4
-        end
-        if(detect[9]>0)then
-            while turnTime < 200 do
-                turnTime=turnTime+1
+            if (detect[9]>0) then
+                estadoDeOperacao="Segue a parede"
             end
-            turnTime=0
-            estadoDeOperacao="Segue a parede"
+        elseif (turnDirection==1) then
+            vLeft=3.4
+            vRight=2.0
+            if (detect[16]>0) then
+                estadoDeOperacao="Segue a parede"
+            end
+        elseif (turnDirection==-1) then
+            vLeft=2.8
+            vRight=3.4
+            if (detect[9]>0) and (detect[9]<0.5) then
+               estadoDeOperacao="Segue a parede"
+            end
         end
     end
     
